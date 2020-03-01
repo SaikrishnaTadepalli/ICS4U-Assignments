181a4c1255b0bc8729dcf773aad28979100eec9f
SaikrishnaTadepalli <58894708+SaikrishnaTadepalli@users.noreply.github.com>
2020-03-01 18:33:58 -0500

    Add files via upload

---
+++ TadepalliS_ASSN02/TadepalliS_ASSN02/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace TadepalliS_ASSN02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lsvOut = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnGenHistory = new System.Windows.Forms.Button();
+            this.btnChkNumbers = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.RosyBrown;
+            this.lblTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTitle.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(750, 32);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Enter your lucky LottoMax #s to check";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(12, 44);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(750, 32);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lsvOut
+            // 
+            this.lsvOut.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lsvOut.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsvOut.HideSelection = false;
+            this.lsvOut.Location = new System.Drawing.Point(12, 82);
+            this.lsvOut.Name = "lsvOut";
+            this.lsvOut.Size = new System.Drawing.Size(750, 250);
+            this.lsvOut.TabIndex = 2;
+            this.lsvOut.UseCompatibleStateImageBehavior = false;
+            this.lsvOut.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Date";
+            this.columnHeader1.Width = 151;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Lotto Max Numbers";
+            this.columnHeader2.Width = 250;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Matches";
+            this.columnHeader3.Width = 151;
+            // 
+            // btnGenHistory
+            // 
+            this.btnGenHistory.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnGenHistory.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenHistory.Location = new System.Drawing.Point(12, 338);
+            this.btnGenHistory.Name = "btnGenHistory";
+            this.btnGenHistory.Size = new System.Drawing.Size(200, 75);
+            this.btnGenHistory.TabIndex = 3;
+            this.btnGenHistory.Text = "Generate Lotto History";
+            this.btnGenHistory.UseVisualStyleBackColor = false;
+            this.btnGenHistory.Click += new System.EventHandler(this.btnGenHistory_Click);
+            // 
+            // btnChkNumbers
+            // 
+            this.btnChkNumbers.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnChkNumbers.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChkNumbers.Location = new System.Drawing.Point(562, 338);
+            this.btnChkNumbers.Name = "btnChkNumbers";
+            this.btnChkNumbers.Size = new System.Drawing.Size(200, 75);
+            this.btnChkNumbers.TabIndex = 4;
+            this.btnChkNumbers.Text = "Check Numbers";
+            this.btnChkNumbers.UseVisualStyleBackColor = false;
+            this.btnChkNumbers.Click += new System.EventHandler(this.btnChkNumbers_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnExit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(12, 419);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(750, 50);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnClear.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(291, 350);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(200, 50);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(776, 484);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnChkNumbers);
+            this.Controls.Add(this.btnGenHistory);
+            this.Controls.Add(this.lsvOut);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "ASSN02 Lucky Numbers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ListView lsvOut;
+        private System.Windows.Forms.Button btnGenHistory;
+        private System.Windows.Forms.Button btnChkNumbers;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ TadepalliS_ASSN02/TadepalliS_ASSN02/Form1.cs
@@ -0,0 +1,192 @@
+﻿/*************************************************************************************
+    PROGRAMME	:	ASSN02 Lucky Numbers
+
+    OUTLINE		:	This programme asks the user to input 7 numbers between 1 and 49 
+                    inclusive. It then generates 1 years worth of lucky numbers
+                    (for every friday), and compairs the entered numbers with the random 
+                    numbers generated. The program lists the matched numbers, if there
+                    are any, for every friday until next years date.
+
+    PROGRAMMER	:	Saikrishna Tadepalli
+
+    DATE		:	October 31, 2019
+ *************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TadepalliS_ASSN02
+{
+    public partial class Form1 : Form
+    {       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EnableObj(false);
+            btnClear.Enabled = false;
+            btnClear.Visible = false;
+        }
+
+        // This method makes the title, textbox to input numbers, and the button to check numbers enable and visible state to the entered boolean parameter
+        private void EnableObj(bool state)
+        {
+            Control[] ctrls = { lblTitle as Control, txtInput as Control, btnChkNumbers as Control };
+            foreach (Control c in ctrls)
+                c.Visible = state;
+            foreach (Control c in ctrls)
+                c.Enabled = state;
+        }
+
+        private void btnGenHistory_Click(object sender, EventArgs e)
+        {
+            StreamWriter SW = new StreamWriter("lotto.txt");
+            MessageBox.Show("File lotto.txt created.","ASSN02",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            generateLottoNumbers(SW);
+            EnableObj(true);
+            txtInput.Focus();
+            btnGenHistory.Enabled = false;
+        }
+        
+        private void btnChkNumbers_Click(object sender, EventArgs e)
+        {
+            if (ErrorChecking())
+                foreach (String s in System.IO.File.ReadAllLines("lotto.txt"))
+                    checkNumbers(s);
+            else
+                MessageBox.Show("Lotto Numbers not in Correct Format!", "ASSN02", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            txtInput.Focus();
+        }
+
+        // This function checks if the user entered 7 unique integers between 1 and 49 inclusive. If so, it returns true, else it retruns false
+        private Boolean ErrorChecking()
+        {
+            string[] entString = txtInput.Text.Trim().Split(Convert.ToChar(" "));
+
+            if (entString.Length == 7)
+            {
+                int[] entNumbers = new int[7];
+
+                for (int i = 0; i < entNumbers.Length; i++)              
+                    if (!(int.TryParse(entString[i], out entNumbers[i])))
+                        return false;
+
+                foreach (int i in entNumbers)
+                    if (i <= 0 || i > 49)
+                        return false;
+                
+                btnClear.Enabled = true;
+                btnClear.Visible = true;
+                
+                return true;
+            }
+            return false;
+        }
+
+        // This function generates lotto numbers for 1 year and writes them to the file "lotto.txt"
+        private void generateLottoNumbers(StreamWriter writer)
+        {
+            DateTime startDate = DateTime.Today;
+            Random rand = new Random();
+
+            while (startDate.DayOfWeek != DayOfWeek.Friday)
+                startDate = startDate.AddDays(1);
+
+            for (int i = 0; i < 52; i++)
+            {
+                string randomNum = "";
+
+                for (int j = 0; j < 7; j++)
+                    randomNum += rand.Next(1, 50).ToString("D2") + " ";
+
+                writer.WriteLine((startDate.AddDays(7 * (i)).ToString("MM-dd-yy")) + " " + randomNum.Trim());
+            }
+            writer.Close();
+        }
+
+        // This method checks each line of the string parameter lottoLine with the entered numbers and outputs the matches
+        private void checkNumbers(string lottoLine)
+        {
+            string date = lottoLine.Substring(0, 8);
+            string numbers = lottoLine.Substring(9, 20);
+            string matches = "";
+
+            string[] inputs = txtInput.Text.Trim().Split(Convert.ToChar(" "));
+
+            for (int i = 0; i < inputs.Length; i++)
+                if (numbers.Split(Convert.ToChar(" "))[i] == (int.Parse(inputs[i])).ToString("D2"))
+                    matches += (int.Parse(inputs[i])).ToString("D2") + " ";
+
+            string[] row1 = { numbers, matches.Trim() };
+            lsvOut.Items.Add(date).SubItems.AddRange(row1);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            EnableObj(false);
+            btnGenHistory.Enabled = true;
+            btnClear.Enabled = false;
+            btnClear.Visible = false;
+            lsvOut.Items.Clear();
+            txtInput.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
+
+// This program allows the users to enter repeating numbers and it generates repeating numbers (because the document dosnt specify to disable repeating numbers ). 
+// The comments below detail how to prevent or enable repeated inputs and repeated numbers generated. 
+
+/*this following code preverts the users from entering repeating numbers, enter the following at line 83 to implement:
+  
+                for (int i = 0; i < entNumbers.Length; i++)
+                    for (int j = i + 1; j < entNumbers.Length; j++)
+                        if (entNumbers[i] == entNumbers[j]) 
+                            return false;
+
+ * To enable users to enter repeating numbers, remove the code snippet above from the source code. 
+
+ */
+
+/* this following code preverts repeating numbers from being generated, replace lines (106-107) with the following to implement:
+ 
+            for (int j = 0; j < 7; j++)
+            {
+                    bool repeat = true;
+                    int num = 0;
+
+                    do
+                    {
+                         num = rand.Next(1, 50);
+
+                        for (int k = 0; k < randomNum.Length; k++)
+                            for (int l = k + 1; l < randomNum.Length; l++)
+                                if (!(randomNum[k] == randomNum[l]))
+                                    repeat = false;
+                    } while (repeat);
+
+                    randomNum += num.ToString("D2") + " ";
+             }
+
+* Alternativly, to enable repeated numbers to be generated, enter the following at line 106 to impliment:
+
+                    for (int j = 0; j < 7; j++)
+                        randomNum += rand.Next(1, 50).ToString("D2") + " ";
+ */
